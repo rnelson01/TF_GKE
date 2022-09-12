a53d0642ecbeb60440c8ee770a260b67a140c969
Ryan Nelson <44874266+rnelson01@users.noreply.github.com>
2022-09-12 15:01:38 -0500

    Update gke-kubeconfig.sh

--- gke-kubeconfig.sh
+++ gke-kubeconfig.sh
@@ -1,1 +1,3 @@
-gcloud container clusters get-credentials $(terraform output kubernetes_cluster_name) --region $(terraform output region)
+gcloud container clusters get-credentials $(terraform output kubernetes_cluster_name) --region $(terraform output region) 
+echo terraform.kubernetes_cluster_name | grep "server:"
+echo terraform.kubernetes_cluster_name | grep "- cluster:"
